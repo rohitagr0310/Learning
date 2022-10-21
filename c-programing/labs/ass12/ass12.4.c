c1e4c6c7b46f137262f4c78e1e87bff546722b95
rohitagr0310 <rohitagr2610@gmail.com>
2022-10-21 23:23:50 +0530

    ll

--- c-programing/labs/ass12/ass12.4.c
+++ c-programing/labs/ass12/ass12.4.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int a,f;
+    int a, f = 1;
     printf("Enter the number you wish to know the factorial of - ");
-    scanf("%d",&a);
+    scanf("%d", &a);
 
-    for (int i = 0; i <= a; i++)
+    for (int i = 1; i <= a; i++)
     {
-        f=f*i;
+        f = f * i;
     }
-    
+
+    printf("%d\n\n", f);
     return 0;
 }
